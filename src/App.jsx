8b960345c2192ba602cf2147778d69bab43c8bbb
chetan-malviyaa <chetanmalviya.2002@gmail.com>
2024-05-16 23:49:13 +0530

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+export default App;
